eda92e01b9f391ace83c4271999b71ab96a63a48
Oxana Lundström <oxana.nu@gmail.com>
2021-11-11 15:17:34 +0100

    Trying a new way to connect to postgres

--- strAPI/repeats/database.py
+++ strAPI/repeats/database.py
@@ -1,10 +1,12 @@
 import os
 from sqlmodel import create_engine, Session
-import psycopg2
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
-engine = psycopg2.connect(DATABASE_URL, sslmode='require')
+# Convert "postgres://<db_address>"  --> "postgresql+psycopg2://<db_address>" needed for SQLAlchemy
+final_db_url = "postgresql+psycopg2://" + DATABASE_URL.lstrip("postgres://")  
+
+engine = create_engine(final_db_url, echo=False)
 
 def get_db():
   with Session(engine) as session:
