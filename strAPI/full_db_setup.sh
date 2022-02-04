e66affdc1c5597d395ce653ee42f4de77d93b320
Oxana Lundström <oxana.nu@gmail.com>
2022-02-04 11:21:47 +0100

    First draft of insert variations in db

--- strAPI/full_db_setup.sh
+++ strAPI/full_db_setup.sh
@@ -6,13 +6,14 @@ set -eou pipefail
 # gtf="/home/oxana/projects/str_database/data/genome_anntotation/gencode.v22.annotation.gtf"
 db="sqlite:////Users/maxverbiest/PhD/projects/str_database/db/example.db"
 gtf="/Users/maxverbiest/PhD/projects/str_database/data/genome_anntotation/chr1_small_gencode.v22.annotation.gtf"
-repeat_dir="/Users/maxverbiest/PhD/projects/str_database/data/repeats"
+repeat_dir="../data/repeats"
+db="postgresql+psycopg2://postgres:Oxanaisnice@localhost:5432/strdb"
 
 echo "Setting up database file"
 python3 setup_db.py --database "${db}"
 
 echo "Adding genes"
-python3 gtf_to_sql.py --gtf "${gtf}" -d "${db}"
+#python3 gtf_to_sql.py --gtf "${gtf}" -d "${db}"
 
 echo "Inserting repeats"
 python3 insert_repeats.py -d "${db}" -r "${repeat_dir}" -s phylo_gap01
---
+++ strAPI/insert_variations.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import argparse
+from gtf_to_sql import connection_setup
+import pandas as pd
+
+from repeats.models import CRCVariation, Repeat
+
+def make_db_variation(df_row, session):
+    try:
+        var_already_in_db = session.query(CRCVariation).filter_by(
+            tcga_barcode=df_row["patient"],
+            sample_type=df_row["sample_type"],
+            repeat_id=df_row["repeat_id"]).one()
+        return "Already exists in the database"
+    except:
+        # initialize instance of database CRCVariation
+        db_variation = CRCVariation(
+            tcga_barcode = df_row["patient"],
+            sample_type = df_row["sample_type"],
+            reference = df_row["ref"],
+            alt = df_row["alt"],
+            repeat_id = df_row["repeat_id"]
+        )
+
+        try:
+            repeat = session.query(Repeat).get(df_row["repeat_id"])
+            repeat.crcvariations.append(db_variation)
+        except:
+            print("This might be a PERF repeat, " + df_row["repeat_id"])
+        return True
+
+def cla_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--database", "-d", type=str, required=True, help="Path to where the repeat-containing database can be found"
+    )
+    parser.add_argument(
+        "--var", "-v", type=str, required=True, help="Path to variations file in csv format"
+    )
+  
+    return parser.parse_args()
+
+
+def main():
+    args = cla_parser()
+    db_path = args.database
+    input_path = args.var
+    print("Connecting to the database")
+    engine, session = connection_setup(db_path)
+
+    df_var = pd.read_csv(input_path)
+
+    df_var.apply(lambda row : make_db_variation(row, session), axis = 1)
+  
+    session.commit()
+
+if __name__ == "__main__":
+    main()
