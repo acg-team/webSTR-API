c83fc14d6596d22e56c807490f6d19557a5c404a
maverbiest <mavh1995@gmail.com>
2022-07-25 14:40:41 +0200

    "Small edits to variables referencing local files needed for setup to work "

--- strAPI/full_db_setup.sh
+++ strAPI/full_db_setup.sh
@@ -4,16 +4,16 @@ set -eou pipefail
 #change password to yours before running
 # db=postgresql+psycopg2://postgres:password@localhost:5432/strdb
 # gtf="/home/oxana/projects/str_database/data/genome_anntotation/gencode.v22.annotation.gtf"
-db="sqlite:////Users/maxverbiest/PhD/projects/str_database/db/example.db"
-gtf="/Users/maxverbiest/PhD/projects/str_database/data/genome_anntotation/chr1_small_gencode.v22.annotation.gtf"
-repeat_dir="../data/repeats"
-db="postgresql+psycopg2://postgres:Oxanaisnice@localhost:5432/strdb"
-
+# db="sqlite:////Users/maxverbiest/PhD/projects/str_database/db/example.db"
+gtf="/Users/maxverbiest/PhD/data/genome_annotations/gencode.v22.annotation.gtf"
+repeat_dir="/Users/maxverbiest/PhD/projects/str_database/data/repeats"
+# db="postgresql+psycopg2://postgres:Oxanaisnice@localhost:5432/strdb"
+db="postgresql+psycopg2://postgres:postgres@localhost:5432/strdb"
 echo "Setting up database file"
-python3 setup_db.py --database "${db}"
+# python3 setup_db.py --database "${db}"
 
 #echo "Adding genes"
-#python3 gtf_to_sql.py --gtf "${gtf}" -d "${db}"
+# python3 gtf_to_sql.py --gtf "${gtf}" -d "${db}"
 
 #echo "Inserting repeats"
-#ython3 insert_repeats.py -d "${db}" -r "${repeat_dir}" -s phylo_gap01
+python3 insert_repeats.py -d "${db}" -r "${repeat_dir}" -s phylo_gap01
--- strAPI/repeats/database.py
+++ strAPI/repeats/database.py
@@ -4,7 +4,8 @@ from sqlmodel import create_engine, Session
 DATABASE_URL = os.environ['DATABASE_URL']
 
 # Convert "postgres://<db_address>"  --> "postgresql+psycopg2://<db_address>" needed for SQLAlchemy
-final_db_url = "postgresql+psycopg2://" + DATABASE_URL.lstrip("postgres://")  
+# final_db_url = "postgresql+psycopg2://" + DATABASE_URL.lstrip("postgres://")  
+final_db_url = DATABASE_URL
 
 engine = create_engine(final_db_url, echo=False)
 
