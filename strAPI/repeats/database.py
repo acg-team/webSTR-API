8a3597599dfe9b9af4c3ced4cbc0072a88874e1e
Oxana (Sachenkova) Lundström <oxana.nu@gmail.com>
2022-10-04 14:02:06 +0200

    Prepare for a new deploy to heroku

--- strAPI/repeats/database.py
+++ strAPI/repeats/database.py
@@ -1,11 +1,10 @@
 import os
 from sqlmodel import create_engine, Session
 
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['HEROKU_POSTGRESQL_ORANGE']
 
 # Convert "postgres://<db_address>"  --> "postgresql+psycopg2://<db_address>" needed for SQLAlchemy
-# final_db_url = "postgresql+psycopg2://" + DATABASE_URL.lstrip("postgres://")  
-final_db_url = DATABASE_URL
+final_db_url = "postgresql+psycopg2://" + DATABASE_URL.lstrip("postgres://")  
 
 engine = create_engine(final_db_url, echo=False)
 
